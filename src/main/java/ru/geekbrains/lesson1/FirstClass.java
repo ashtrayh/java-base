179e5677a8539d0c2e6a23b4b6809716bf976dec
Darya <QqPpsldkcm10>
2021-03-15 01:43:10 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson1/FirstClass.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.lesson1;
+
+public class FirstClass {
+    // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
+    public static void main(String[] args) {
+        // 2. Создать переменные всех пройденных типов данных и инициализировать их значения.
+byte byteValue = -50;
+short shortValue = 31457;
+int intValue = -1235684;
+long longValue = 8000000L;
+float floatValue;
+floatValue = 456.32f;
+double doubleValue = -8978.365;
+char charValue = 's';
+boolean blooleanValue = true;
+    }
+}
